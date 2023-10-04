bb0f0ca7a5fecd53bad7e9390d8c5656477f7f90
bayu <bayuferdiman1@gmail.com>
2023-10-04 19:28:43 +0700

    Update Hero Animation

--- src/views/LandingPage/components/Hero.js
+++ src/views/LandingPage/components/Hero.js
@@ -1,10 +1,11 @@
 import { Flex, Text, Box, Badge, useColorModeValue } from '@chakra-ui/react';
+import { motion } from 'framer-motion';
 
 const Hero = () => {
   const bgImage =
     'https://images.unsplash.com/photo-1588668214407-6ea9a6d8c272?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80';
   const badgeColorScheme = 'blue';
-  const badgeFontSize = "0.8em"
+  const badgeFontSize = '0.8em';
 
   return (
     <Flex
@@ -21,42 +22,86 @@ const Hero = () => {
         boxSize={{ base: 'fit-content', md: 'fit-content', lg: 'max-content' }}
         textAlign="center"
       >
-        <Text
-          fontSize={{ base: '30px', md: '70px', lg: '110px' }}
-          fontFamily="heading"
-          fontWeight="900"
-          color="gray.50"
-          mb={{ base: '10px', md: '20px' }}
-          //   textShadow="1px 1px 2px rgba(0, 0, 0, 66)"
-          // boxShadow={"xl"}
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{
+            ease: 'easeInOut',
+            duration: 2,
+          }}
         >
-          PRASAJA NGESTI WIBAWA
-        </Text>
+          <Text
+            fontSize={{ base: '30px', md: '70px', lg: '110px' }}
+            fontFamily="heading"
+            fontWeight="900"
+            color="gray.50"
+            mb={{ base: '10px', md: '20px' }}
+          >
+            PRASAJA NGESTI WIBAWA
+          </Text>
+        </motion.div>
+
+        <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{
+          ease: 'easeInOut',
+          duration: 5,
+        }}
+        >
+
         <Text
           fontFamily="heading"
           color="gray.50"
           fontSize={{ base: 'sm', md: 'md', lg: 'lg' }}
         >
           Terwujudnya Kabupaten Probolinggo Yang
-          <Badge variant="subtle" colorScheme={badgeColorScheme} fontSize={badgeFontSize} m={"0px 2px"}>
+          <Badge
+            variant="subtle"
+            colorScheme={badgeColorScheme}
+            fontSize={badgeFontSize}
+            m={'0px 2px'}
+          >
             Sejahtera,
           </Badge>
-          <Badge variant="subtle" colorScheme={badgeColorScheme} fontSize={badgeFontSize} m={"0px 2px"}>
+          <Badge
+            variant="subtle"
+            colorScheme={badgeColorScheme}
+            fontSize={badgeFontSize}
+            m={'0px 2px'}
+          >
             Berkeadilan
           </Badge>
           ,
-          <Badge variant="subtle" colorScheme={badgeColorScheme} fontSize={badgeFontSize} m={"0px 2px"}>
+          <Badge
+            variant="subtle"
+            colorScheme={badgeColorScheme}
+            fontSize={badgeFontSize}
+            m={'0px 2px'}
+          >
             Mandiri
           </Badge>
           ,
-          <Badge variant="subtle" colorScheme={badgeColorScheme} fontSize={badgeFontSize} m={"0px 2px"}>
+          <Badge
+            variant="subtle"
+            colorScheme={badgeColorScheme}
+            fontSize={badgeFontSize}
+            m={'0px 2px'}
+          >
             Berwawasan Lingkungan
           </Badge>
           dan
-          <Badge variant="subtle" colorScheme={badgeColorScheme} fontSize={badgeFontSize} m={"0px 2px"}>
+          <Badge
+            variant="subtle"
+            colorScheme={badgeColorScheme}
+            fontSize={badgeFontSize}
+            m={'0px 2px'}
+          >
             Berakhlak Mulia.
           </Badge>
         </Text>
+        </motion.div>
+
       </Box>
     </Flex>
   );
